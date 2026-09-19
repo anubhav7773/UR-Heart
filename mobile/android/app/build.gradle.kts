00ea7fa16e74f81623fa680ff5d61ac8bbec1b8f
Anubhav <kshtriyaanubhav9120@gmail.com>
2026-09-19 13:21:10 +0530

    fix(android): resolve JVM target compatibility and enable desugaring for release build

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -11,6 +11,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -49,6 +50,7 @@ flutter {
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
 }
--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -27,6 +27,15 @@ subprojects {
             freeCompilerArgs.add("-Xsuppress-version-warnings")
         }
     }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
 }
 
 tasks.register<Delete>("clean") {
